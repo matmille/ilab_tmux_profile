7b7ce01b171b88b62b19072d80403492f5e216c0
matmille <matmille@redhat.com>
2024-07-01 10:21:21 -0400

    Update tmux-rhelai-shell.sh

--- tmux-rhelai-shell.sh
+++ tmux-rhelai-shell.sh
@@ -6,7 +6,7 @@ tmux new-session -d -s $SESSION
 tmux split-window -h -t $SESSION:
 tmux split-window -v -t $SESSION:
 tmux resize-pane -R 20
-tmux send-keys -t $SESSION:0.1 "watch -n5 nvidia-smi --query-gpu=name,utilization.gpu,utilization.memory --format=csv" Enter
+tmux send-keys -t $SESSION:0.1 "nvtop" Enter
 tmux send-keys -t $SESSION:0.2 'watch -n5 "top -bn 1 | grep \"^ \" | tr -s \" \"  | cut -d\" \" -f2,10,11 | column -s\" \" -t | head -10"' Enter
 tmux select-pane -t $SESSION:0.0
 tmux attach-session -t $SESSION
