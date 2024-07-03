ec07bae91f1c5e56f4f3d2982b7c87c1d6556831
matmille <matmille@redhat.com>
2024-07-03 16:24:15 -0400

    Update tmux-rhelai-shell.sh

--- tmux-rhelai-shell.sh
+++ tmux-rhelai-shell.sh
@@ -7,6 +7,6 @@ tmux split-window -h -t $SESSION:
 tmux split-window -v -t $SESSION:
 tmux resize-pane -R 20
 tmux send-keys -t $SESSION:0.1 "nvtop" Enter
-tmux send-keys -t $SESSION:0.2 'watch -n5 "top -bn 1 | grep \"^ \" | tr -s \" \"  | cut -d\" \" -f2,10,11 | column -s\" \" -t | head -10"' Enter
+tmux send-keys -t $SESSION:0.2 'top' Enter
 tmux select-pane -t $SESSION:0.0
 tmux attach-session -t $SESSION
