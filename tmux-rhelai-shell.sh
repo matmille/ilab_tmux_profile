3dbfc629e2ab74e4c7b1b08f860c00c1837ca31e
Matthew Miller <matmille@redhat.com>
2024-07-13 18:27:33 -0400

    Added ^O

--- tmux-rhelai-shell.sh
+++ tmux-rhelai-shell.sh
@@ -10,6 +10,8 @@ tmux split-window -v -t $SESSION:
 tmux resize-pane -R 20
 tmux send-keys -t $SESSION:0.0 "mc -x" Enter
 sleep 1e-06
+tmux send-keys -t $SESSION:0.0 "" Enter
+sleep 1e-06
 tmux send-keys -t $SESSION:0.0 "cd ~/instructlab-latest" Enter
 sleep 1e-06
 tmux send-keys -t $SESSION:0.0 "source venv/bin/activate" Enter
